56dc47f82c8e13bd8f83177c481418e666a3a05e
keyuanwu <wky0702@gmail.com>
2020-04-05 21:48:40 +0800

    2020/04/05

--- easy/771. Jewels and Stones.py
+++ easy/771. Jewels and Stones.py
@@ -17,7 +17,6 @@ S and J will consist of letters and have length at most 50.
 The characters in J are distinct.
 '''
 
-
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         return sum((s in J for s in S))
